151cb9528d03ccebf3345a1cec9ca52144405086
Moritz Bastigkeit <moritz.bastigkeit@hotmail.com>
2017-06-01 08:40:04 +0200

    commit

---
+++ src/main/java/at/htld/module/co2/Bsp17Co2Application.java
@@ -0,0 +1,12 @@
+package at.htld.module.co2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bsp17Co2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bsp17Co2Application.class, args);
+	}
+}
